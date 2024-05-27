b30766d5bc122956f7eaf2547913fe69a92a0c13
punkyard <59349105+punkyard@users.noreply.github.com>
2024-05-28 00:36:10 +0200

    Create rez0-debian-fisrtb00t.sh
    first file for lerez0

---
+++ rez0-debian-fisrtb00t.sh
@@ -0,0 +1,6 @@
+#!/bin/bash # ce shebang indique au serveur quel lanceur de commande utiliser pour exécuter ce script
+
+# ce script DOIT être modifié avec les informations propres à votre organisation et votre serveur avant d'être exécuté
+# pour modifier ce script, utiliser la commande : nano rez0-debian-firstboot-init-script-fr.sh depuis le dossier de votre serveur où le fichier a été créé
+
+# ce script peut aussi être copié-collé et modifié dans un fichier nommé rez0-debian-firstboot-init-script-fr.sh (par exemple), dans le dossier /home de l'utilisateur root avec les permissions (...)
